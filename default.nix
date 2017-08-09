678fc4099baec72e36c3e8405fd6085fd57ffc33
Matthew Bauer <mjbauer95@gmail.com>
2017-08-09 05:46:49 +0000

    don’t use scheme-full
    we don’t need everything in there

--- default.nix
+++ default.nix
@@ -7,8 +7,13 @@ stdenv.mkDerivation {
   src = ./.;
   buildInputs = [
     (texlive.combine {
-      inherit (texlive) scheme-full xetex setspace fontspec
-                        chktex enumitem xifthen ifmtarg filehook;
+      inherit (texlive) scheme-basic xetex xetex-def setspace fontspec
+                        chktex enumitem xifthen ifmtarg filehook
+                        upquote tools ms geometry graphics oberdiek
+                        fancyhdr lastpage xcolor etoolbox unicode-math
+                        ucharcat sourcesanspro tcolorbox pgf environ
+                        trimspaces parskip hyperref url euenc
+                        collection-fontsrecommended;
     })
   ];
   buildPhase = ''
